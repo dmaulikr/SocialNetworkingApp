f9916aec802a2c29dfcfac81a6fab82d2a7f3fb7
Juan Alberto Aranda Alvarez <juan.arandaalvarez@gmail.com>
2016-01-04 16:47:18 -0800

    Initial Commit

---
+++ SocialNetworkingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialNetworkingApp
+//
+//  Created by Juan Aranda-Alvarez on 1/4/16.
+//  Copyright © 2016 CodigoAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SocialNetworkingApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SocialNetworkingApp
+//
+//  Created by Juan Aranda-Alvarez on 1/4/16.
+//  Copyright © 2016 CodigoAND. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
